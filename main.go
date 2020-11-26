2d29a1f3aaf5ec9004a776c27f957cef4626a5c6
yoyofx <zlhxd@hotmail.com>
2020-11-26 15:04:56 +0800

    没有配置不能指定view目录

--- main.go
+++ main.go
@@ -8,15 +8,17 @@ import (
 )
 
 func main() {
-
 	webHost := CreateYoyoBlogBuilder().Build()
 	webHost.Run()
 }
 
 func CreateYoyoBlogBuilder() *Abstractions.HostBuilder {
+	configuration := Abstractions.NewConfigurationBuilder().AddYamlFile("config").Build()
 	return YoyoGo.NewWebHostBuilder().
+		UseConfiguration(configuration).
 		Configure(func(app *YoyoGo.WebApplicationBuilder) {
 			app.UseMvc(func(builder *Mvc.ControllerBuilder) {
+				builder.AddViewsByConfig()
 				builder.AddController(controller.NewUserController)
 			})
 		})
